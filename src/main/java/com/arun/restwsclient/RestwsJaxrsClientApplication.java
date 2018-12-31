e96c0c45b0892e152460bde2b6869592020223d2
ArunKumarBanala <banalaarun1@gmail.com>
2018-12-30 19:30:07 -0500

    first commit

---
+++ src/main/java/com/arun/restwsclient/RestwsJaxrsClientApplication.java
@@ -0,0 +1,14 @@
+package com.arun.restwsclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestwsJaxrsClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestwsJaxrsClientApplication.class, args);
+	}
+
+}
+
